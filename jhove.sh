55301e77b20cd94c6aa931f488d49b36dc4c31f8
moseshll <moseshll@umich.edu>
2021-09-30 16:45:13 -0400

    Remove shell scripts

--- jhove.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BUNDLE_GEMFILE="$SCRIPT_DIR"/Gemfile
-TEMP_HOME="$SCRIPT_DIR"/.bundle
-HOME=$TEMP_HOME BUNDLE_GEMFILE=$BUNDLE_GEMFILE bundle exec "$SCRIPT_DIR"/jhove.rb $*
--- query.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-BUNDLE_GEMFILE="$SCRIPT_DIR"/Gemfile
-TEMP_HOME="$SCRIPT_DIR"/.bundle
-HOME=$TEMP_HOME RBENV_VERSION=$RBENV_VERSION BUNDLE_GEMFILE=$BUNDLE_GEMFILE bundle exec "$SCRIPT_DIR"/query.rb $*
